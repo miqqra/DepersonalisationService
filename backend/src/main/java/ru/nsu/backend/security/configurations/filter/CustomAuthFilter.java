2c47b32ea539380ace7191dd063efbeadae4a5dc
MihailCCfit <m.tsukanov@g.nsu.ru>
2023-02-28 22:39:42 +0700

    add useless logout, fix time

--- backend/src/main/java/ru/nsu/backend/security/configurations/filter/CustomAuthFilter.java
+++ backend/src/main/java/ru/nsu/backend/security/configurations/filter/CustomAuthFilter.java
@@ -77,14 +77,14 @@ public class CustomAuthFilter extends UsernamePasswordAuthenticationFilter {
         Algorithm algorithm = Algorithm.HMAC256(secretWord.getBytes());
         String token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 15 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities()
                         .stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
         String refresh_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 5 * 60 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 48 * 60 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities()
                         .stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
