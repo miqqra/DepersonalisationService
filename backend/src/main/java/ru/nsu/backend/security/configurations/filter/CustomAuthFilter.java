41e95b6a03921cc279315806a2aa7482136d0fdc
MihailCCfit <m.tsukanov@g.nsu.ru>
2023-03-01 02:14:55 +0700

    token 15 sec

--- backend/src/main/java/ru/nsu/backend/security/configurations/filter/CustomAuthFilter.java
+++ backend/src/main/java/ru/nsu/backend/security/configurations/filter/CustomAuthFilter.java
@@ -77,7 +77,7 @@ public class CustomAuthFilter extends UsernamePasswordAuthenticationFilter {
         Algorithm algorithm = Algorithm.HMAC256(secretWord.getBytes());
         String token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 15 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities()
                         .stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
